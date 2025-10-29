00832a9f8e2495c555dea2abef0823ddda5eee42
lauramunozn0 <lauramunozlaw@gmail.com>
2025-10-28 20:50:03 -0400

    Create the attributes,constructor,g and s and to string and got a better formula.

---
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,63 @@
+package com.pluralsight;
+
+public class Vehicle {
+    private int vin;
+    private int year;
+    private String make;
+    private String model;
+    private String type;
+    private String color;
+    private int odometer;
+    private double price;
+
+    public Vehicle(int vin, int year, String make, String model, String type, String color, double price, int odometer) {
+        this.vin = vin;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.type = type;
+        this.color = color;
+        this.price = price;
+        this.odometer = odometer;
+    }
+
+    public int getVin() {return vin;}
+
+    public void setVin(int vin) {this.vin = vin;}
+
+    public int getYear() {return year;}
+
+    public void setYear(int year) {this.year = year;}
+
+    public String getMake() {return make;}
+
+    public void setMake(String make) {this.make = make;}
+
+    public String getModel() {return model;}
+
+    public void setModel(String model) {this.model = model;}
+
+    public String getColor() {return color;}
+
+    public void setColor(String color) {this.color = color;}
+
+    public String getType() {return type;}
+
+    public void setType(String type) {this.type = type;}
+
+    public int getOdometer() {return odometer;}
+
+    public void setOdometer(int odometer) {this.odometer = odometer;}
+
+    public double getPrice() {return price;}
+
+    public void setPrice(double price) {this.price = price;}
+
+    @Override
+    public String toString() {
+        return String.format("%d %s %s (%s) - %,d miles - $%,.2f",
+                year, make, model, color, odometer, price);
+    }
+}
+
+
