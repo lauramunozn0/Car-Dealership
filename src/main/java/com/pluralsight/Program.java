cbfc4e5b5ff641fa906d9d76677a7ee8a3ce4db0
lauramunozn0 <lauramunozlaw@gmail.com>
2025-10-29 20:07:01 -0400

    added bufferrreader to open the csv file

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,15 +1,37 @@
 package com.pluralsight;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Program {
-    public static void main(String[] args) {
-        Dealership dealership = new Dealership();
+    public static void main(String[] args) throws FileNotFoundException {
 
-        Vehicle car1 = new Vehicle();
+        ArrayList<Vehicle> inventory = new ArrayList<>();
 
-        dealership.addVehicle(car1);
+        try (BufferedReader reader = new BufferedReader(new FileReader("dealership.csv"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split("\\|");
 
-        dealership.showInventory();
+                int vin = Integer.parseInt(data[0]);
+                int year = Integer.parseInt(data[1]);
+                String make = data[2];
+                String model = data[3];
+                String type = data[4];
+                String color = data[5];
+                int odometer = Integer.parseInt(data[6]);
+                double price = Double.parseDouble(data[7]);
+
+                Vehicle vehicle = new Vehicle(vin, year, make, model, type, color, odometer, price);
+                inventory.add(vehicle);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
 
 
     }
 }
+
